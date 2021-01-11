4f53d65a57cd74b66ee99c52df5ee20f8c2618b5
elenamagay <elenamagay1985@gmail.com>
2021-01-10 23:45:46 -0500

    files added

---
+++ Assets/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $("#currentDate").text(moment().format("MMMM Do YYYY"));
+
+    function currentTime() {
+        var realTime = moment().hour()
+        $("textarea").each(function (){
+        var currTime = parseInt($(this).attr("id"));
+
+            if (currTime < realTime) {
+                $(this).removeClass("present future");
+                $(this).addClass("past");
+            } else if (currTime > realTime) {
+                $(this).removeClass("past present");
+                $(this).addClass("future");
+            } else {
+                $(this).removeClass("past future");
+                $(this).addClass("present");
+            }    
+        })
+    }   
+
+    currentTime();
+
+    function toLocal () {
+    var eventToSave = $(this).prev().val();
+        
+        localStorage.getItem("event", eventToSave);
+    };
+
+    toLocal ();
+
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault ();
+
+        var eventDescription = $(".eventDescription");
+        
+        for (var i = 0; i < eventDescription.length; i++) {
+            var savedEvent = [];
+            savedEvent.push(eventDescription.val());
+            localStorage.setItem("event", savedEvent)
+            
+        }
+    })
+
+})
